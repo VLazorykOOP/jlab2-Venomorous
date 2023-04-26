79c2846aa97d6b15e5cc83b0690ad78ac1f31930
Venomorous <94299601+Venomorous@users.noreply.github.com>
2023-04-26 12:10:34 +0300

    Reworked class Rational so it now has user input

--- src/Rational.java
+++ src/Rational.java
@@ -4,21 +4,16 @@ public class Rational {
     private int numerator;
     private int denominator;
 
-    Scanner scan = new Scanner(System.in);
-
-
 
     public Rational(int numerator, int denominator) {
         this.numerator = numerator;
-        if(denominator > 0){
-            this.denominator = denominator;
+        this.denominator = denominator;
         }
-        else{
-            System.out.println("Denominator is less or equal 0");
-        }
-//        reduce();
-    }
 
+    public void setValues(int numerator, int denominator) {
+        this.numerator = numerator;
+        this.denominator = denominator;
+    }
     public Rational add(Rational other) {
         int lcm = lcm(this.denominator, other.denominator);
         int numerator = (this.numerator * (lcm / this.denominator)) +
@@ -70,6 +65,55 @@ public class Rational {
         this.denominator /= gcd;
     }
 
+    public void print(){
+        if(this.numerator != 0) {
+            System.out.print(numerator + "/" + denominator);
+        }else{
+            System.out.println("0");
+        }
+
+    }
+
+    public static void printAdd(Rational fraction1, Rational fraction2) {
+        fraction1.print();
+        System.out.print(" + ");
+        fraction2.print();
+        System.out.print(" = ");
+        Rational result = fraction1.add(fraction2);
+        result.print();
+        System.out.println();
+    }
+
+    public static void printSub(Rational fraction1, Rational fraction2) {
+        fraction1.print();
+        System.out.print(" - ");
+        fraction2.print();
+        System.out.print(" = ");
+        Rational result = fraction1.subtract(fraction2);
+        result.print();
+        System.out.println();
+    }
+
+    public static void printMult(Rational fraction1, Rational fraction2) {
+        fraction1.print();
+        System.out.print(" * ");
+        fraction2.print();
+        System.out.print(" = ");
+        Rational result = fraction1.multiply(fraction2);
+        result.print();
+        System.out.println();
+    }
+
+    public static void printDiv(Rational fraction1, Rational fraction2) {
+        fraction1.print();
+        System.out.print(" / ");
+        fraction2.print();
+        System.out.print(" = ");
+        Rational result = fraction1.divide(fraction2);
+        result.print();
+        System.out.println();
+    }
+
     private int gcd(int a, int b) {
         if (b == 0) {
             return a;
@@ -83,43 +127,247 @@ public class Rational {
 
 
 public static void main(String[] args){
-        Rational fraction1 = new Rational(1, 3);
-        Rational fraction2 = new Rational (3, 5);
-        Rational fraction3 = new Rational(12,16);
-        Rational fraction4 = new Rational(12,0);
+        Scanner scan = new Scanner(System.in);
 
-        if(fraction1.equals(fraction2)){
-            System.out.println("Fraction 1 equals fraction 2");
-        }else{
-            System.out.println("Fraction 1 doesn't equal fraction 2");
+        Rational fraction1 = new Rational(1,1);
+        Rational fraction2 = new Rational(1,1);
+
+        int a =0, b = 0;
+
+        int badInput=0;
+
+        while(badInput!=3){
+            System.out.println("------------------------------");
+            System.out.println("1. Subtract one number from another");
+            System.out.println("2. Add one number to another");
+            System.out.println("3. Multiply one number by another");
+            System.out.println("4. Divide one number by another");
+            System.out.println("5. Exit");
+            System.out.println("Choose an option:");
+            int opt = scan.nextInt();
+            switch (opt) {
+                case 1:
+                    badInput = 0;
+                    System.out.println("Enter a numerator and a denominator for the first number:");
+                        if(scan.hasNextInt()) {
+                            a = scan.nextInt();
+                                while(badInput!=3) {
+                                    if(scan.hasNextInt()){
+                                    b = scan.nextInt();
+                                    if(b > 0){
+                                        fraction1 = new Rational(a,b);
+                                        fraction1.print();
+                                        System.out.println("");
+                                        break;
+                                    }else {
+                                        System.out.println("Denominator can't be less or equal to 0");
+                                        System.out.println("Enter a denominator again:");
+                                        badInput++;
+                                    }
+                                    }else {
+                                        System.out.println("Wrong input");
+                                        badInput++;
+                                        scan.next();
+                                    }
+                            }
+                                System.out.println("Enter a numerator and a denominator for the second number:");
+                                if(scan.hasNextInt()) {
+                                    a = scan.nextInt();
+                                    if(scan.hasNextInt()){
+                                        b = scan.nextInt();
+                                        if(b > 0){
+                                            fraction2 = new Rational(a,b);
+                                            fraction2.print();
+                                            System.out.println("");
+                                            printSub(fraction1,fraction2);
+                                        }else {
+                                            System.out.println("Denominator can't be less or equal to 0");
+                                            badInput++;
+                                        }
+                                    }else {
+                                        System.out.println("Wrong input");
+                                        badInput++;
+                                        scan.next();
+                                    }
+                                }else {
+                                    System.out.println("Wrong input");
+                                    badInput++;
+                                    scan.next();
+                                }
+                    }else {
+                        System.out.println("Wrong input");
+                        badInput++;
+                        scan.next();
+                    }
+                    break;
+                case 2:
+                    badInput = 0;
+                    System.out.println("Enter a numerator and a denominator for the first number:");
+                    if(scan.hasNextInt()) {
+                        a = scan.nextInt();
+                        while(badInput!=3) {
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction1 = new Rational(a,b);
+                                    fraction1.print();
+                                    System.out.println("");
+                                    break;
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    System.out.println("Enter a denominator again:");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }
+                        System.out.println("Enter a numerator and a denominator for the second number:");
+                        if(scan.hasNextInt()) {
+                            a = scan.nextInt();
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction2 = new Rational(a,b);
+                                    fraction2.print();
+                                    System.out.println("");
+                                    printAdd(fraction1,fraction2);
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }else {
+                            System.out.println("Wrong input");
+                            badInput++;
+                            scan.next();
+                        }
+                    }else {
+                        System.out.println("Wrong input");
+                        badInput++;
+                        scan.next();
+                    }
+                    break;
+                case 3:
+                    badInput = 0;
+                    System.out.println("Enter a numerator and a denominator for the first number:");
+                    if(scan.hasNextInt()) {
+                        a = scan.nextInt();
+                        while(badInput!=3) {
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction1 = new Rational(a,b);
+                                    fraction1.print();
+                                    System.out.println("");
+                                    break;
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    System.out.println("Enter a denominator again:");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }
+                        System.out.println("Enter a numerator and a denominator for the second number:");
+                        if(scan.hasNextInt()) {
+                            a = scan.nextInt();
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction2 = new Rational(a,b);
+                                    fraction2.print();
+                                    System.out.println("");
+                                    printMult(fraction1,fraction2);
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }else {
+                            System.out.println("Wrong input");
+                            badInput++;
+                            scan.next();
+                        }
+                    }else {
+                        System.out.println("Wrong input");
+                        badInput++;
+                        scan.next();
+                    }
+                    break;
+                case 4:
+                    badInput = 0;
+                    System.out.println("Enter a numerator and a denominator for the first number:");
+                    if(scan.hasNextInt()) {
+                        a = scan.nextInt();
+                        while(badInput!=3) {
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction1 = new Rational(a,b);
+                                    fraction1.print();
+                                    System.out.println("");
+                                    break;
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    System.out.println("Enter a denominator again:");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }
+                        System.out.println("Enter a numerator and a denominator for the second number:");
+                        if(scan.hasNextInt()) {
+                            a = scan.nextInt();
+                            if(scan.hasNextInt()){
+                                b = scan.nextInt();
+                                if(b > 0){
+                                    fraction2 = new Rational(a,b);
+                                    fraction2.print();
+                                    System.out.println("");
+                                    printDiv(fraction1,fraction2);
+                                }else {
+                                    System.out.println("Denominator can't be less or equal to 0");
+                                    badInput++;
+                                }
+                            }else {
+                                System.out.println("Wrong input");
+                                badInput++;
+                                scan.next();
+                            }
+                        }else {
+                            System.out.println("Wrong input");
+                            badInput++;
+                            scan.next();
+                        }
+                    }else {
+                        System.out.println("Wrong input");
+                        badInput++;
+                        scan.next();
+                    }
+                    break;
+                case 5:
+                    System.exit(0);
+                default:
+                    System.out.println("There is no option for this number \n");
+            }
         }
-        if(fraction1.isGreaterThan(fraction3)){
-            System.out.println("Fraction 1 is greater than fraction 3");
-        }else{
-            System.out.println("Fraction 1 is less than fraction 2");
-        }
-        if(fraction3.isLessThan(fraction1)){
-            System.out.println("Fraction 3 is less than fraction 1");
-        }else{
-            System.out.println("Fraction 3 is greater than fraction 1");
-        }
 
-        System.out.println(fraction1.numerator + "/" + fraction1.denominator);
-        System.out.println(fraction2.numerator + "/" + fraction2.denominator);
-        System.out.println(fraction3.numerator + "/" + fraction3.denominator);
-
-        Rational Add = fraction1.add(fraction2);
-        System.out.println(fraction1.numerator + "/" + fraction1.denominator + " + " + fraction2.numerator + "/" + fraction2.denominator + " = " + Add.numerator + "/" + Add.denominator);
-
-        Rational sub = fraction3.subtract(fraction1);
-        System.out.println(fraction3.numerator + "/" + fraction3.denominator + " - " + fraction1.numerator + "/" + fraction1.denominator + " = " + sub.numerator + "/" + sub.denominator);
-
-        Rational Mult = fraction1.multiply(fraction3);
-        System.out.println(fraction1.numerator + "/" + fraction1.denominator + " * " + fraction3.numerator + "/" + fraction3.denominator + " = " + Mult.numerator + "/" + Mult.denominator);
-
-        Rational Div = fraction2.divide(fraction3);
-        System.out.println(fraction2.numerator + "/" + fraction2.denominator + " : " + fraction3.numerator + "/" + fraction3.denominator + " = " + Div.numerator + "/" + Div.denominator);
-
-    System.out.println("Fraction 4: " + fraction4.denominator);
-    }
+}
 }
